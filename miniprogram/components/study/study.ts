75777dfbedab3f80afa83789ab20c44fe19ab8ac
jxb <i@Jxb.name>
2022-09-03 19:04:27 +0800

    add mp3

--- miniprogram/components/study/study.ts
+++ miniprogram/components/study/study.ts
@@ -38,9 +38,9 @@ Component({
                 const r = Math.random();
                 const left = (r * (Number(config.leftMax) - Number(config.leftMin)) + Number(config.leftMin)).toFixed(0);
                 const right = (r * (Number(config.rightMax) - Number(config.rightMin)) + Number(config.rightMin)).toFixed(0);
-                
+
                 questions.push({
-                    left: left, 
+                    left: left,
                     right: right
                 })
             }
@@ -57,29 +57,44 @@ Component({
      * 组件的方法列表
      */
     methods: {
-        onAnswerChnage: function(e) {
+        onAnswerChnage: function (e) {
             const answer = e.detail.value;
             this.setData({
                 answer: answer
             })
-        } ,
-        onSubmit: function() {
+        },
+        onSubmit: function () {
             const qa = this.data.questions[this.data.index];
             const answer = Number(qa.left) + Number(qa.right);
             if (answer != Number(this.data.answer)) {
-                console.log(answer)
-                console.log(this.data.answer)
-                wx.showToast({
-                    title: '答错了',
-                    icon: 'error'
+                // console.log(answer)
+                // console.log(this.data.answer)
+                // wx.showToast({
+                //     title: '答错了',
+                //     icon: 'error'
+                // })
+                const innerAudioContext = wx.createInnerAudioContext({
+                    useWebAudioImplement: false // 是否使用 WebAudio 作为底层音频驱动，默认关闭。对于短音频、播放频繁的音频建议开启此选项，开启后将获得更优的性能表现。由于开启此选项后也会带来一定的内存增长，因此对于长音频建议关闭此选项
                 })
+                innerAudioContext.src = '/mp3/failure.mp3'
+                innerAudioContext.autoplay = true;
+                innerAudioContext.play()
+                this.setData({
+                    answer: ""
+                });
                 return
-            }  
-            wx.showToast({
-                title: '答对了',
-                icon: 'success'
+            }
+            // wx.showToast({
+            //     title: '答对了',
+            //     icon: 'success'
+            // })
+
+            const innerAudioContext = wx.createInnerAudioContext({
+                useWebAudioImplement: false // 是否使用 WebAudio 作为底层音频驱动，默认关闭。对于短音频、播放频繁的音频建议开启此选项，开启后将获得更优的性能表现。由于开启此选项后也会带来一定的内存增长，因此对于长音频建议关闭此选项
             })
-            
+            innerAudioContext.src = '/mp3/success.mp3'
+            innerAudioContext.autoplay = true;
+            innerAudioContext.play()
 
             const index = this.data.index + 1;
             if (index >= this.data.questions.length) {
